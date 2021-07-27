d379bc90912f3c0a76db96b1fe1d6c13821000e4
Riasad Rion <mdrsdrion@gmail.com>
2021-07-27 22:03:15 +0600

    firebase api added

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,16 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 function NewMeetupPage() {
     function addMeetupHandler(meetupData){
-        
+        fetch(
+            'https://react-meetup-f67f5-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
     }
     return (
         <div>

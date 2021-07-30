f58f9bee325cb142edc852c5d2e49e6bb779ac57
Riasad Rion <mdrsdrion@gmail.com>
2021-07-30 15:53:00 +0600

    Unhandled Rejection

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,6 +1,28 @@
-import { Link } from 'react-router-dom'
-import Card from '../ui/Card'
+import { useContext } from 'react';
+import FavoritesContext from '../../store/favorites-context';
+import { Link } from 'react-router-dom';
+import Card from '../ui/Card';
+
+
+
 function MeetupItem(props) {
+    const favoritesCtx = useContext(FavoritesContext);
+    const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+
+    function toggleFavoriteStatusHandler(){
+        if (itemIsFavorite){
+            favoritesCtx.removeFavorite(props.meetup.id)
+        }else{
+            favoritesCtx.addFavorite({
+                id: props.meetup.id,
+                title: props.meetup.title,
+                descr: props.meetup.descr,
+                image: props.meetup.image,
+                time: props.meetup.time,
+            })
+        }
+    }
+
     return (
         <div className="col-4 mb-4">
             <Card>
@@ -8,7 +30,7 @@ function MeetupItem(props) {
                 <div className="card-body">
                     <h5 className="card-title">{props.meetup.title}</h5>
                     <p className="card-text">{props.meetup.time}</p>
-                    <Link  to="/meetup-details" className="btn btn-primary">Go somewhere</Link>
+                    <Link  to="/meetup-details" onClick={toggleFavoriteStatusHandler} className="btn btn-primary">{itemIsFavorite ? 'Remove from favorites': 'Favorites'}</Link>
                 </div>
             </Card>
         </div>
